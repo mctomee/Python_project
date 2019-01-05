0a04acbb5444ee8d1f66bffd631f35b767a03e7e
mctomee <46408117+mctomee@users.noreply.github.com>
2019-01-06 00:26:53 +0100

    Bugfix

--- git_test.py
+++ git_test.py
@@ -1,5 +1,5 @@
 #Csak azért írom ezt a kódot, hogy kipróbáljam a GIT-et.
 x = input("Adj be egy számot")
 print(3*int(x))
-if int(x)<10 && int(x)<0:
+if int(x)<10 and int(x)<0:
     print("Ez csak egy egyjegyű szám.. :)")
